93fee96a712f36cc2420508e5b0f72de50904bf2
Bobbi Tarkany <bl.tarkany@gmail.com>
2023-06-02 12:19:10 -0700

    setup initial user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,4 +11,23 @@ const {
   removeFriend,
 } = require("../../controllers/userControllers");
 
+// login a user
+router.route("/login").post(login);
+
+// signup a user
+router.route("/signup").post(signup);
+
+// get all users
+router.route("/").get(getUsers);
+
+// get user by id, update user by id, delete user by id
+router
+  .route("/:id")
+  .get(getUserById)
+  .put(updateUserById)
+  .delete(deleteUserById);
+
+// add friend, remove friend
+router.route("/:id/friends/:friendId").post(addFriend).delete(removeFriend);
+
 module.exports = router;
